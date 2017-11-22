6f01d1fa926f8a8ac529782ad069642b67a38198
Tadeáš Erban <tad.erban@seznam.cz>
2017-11-22 20:25:15 +0100

    Opravena pyramida

--- Pyramida.java
+++ Pyramida.java
@@ -4,9 +4,12 @@ public class Pyramida {
 		int vyska = Integer.parseInt(args[0]);
 		
 		for (int y = 0; y < vyska; y++) {
-			for (int x = 0; x < y * 2 + 1; x++) {
-				System.out.printf("-");
-			}
+      for (int z = 0; z < vyska - y; z++) {
+          System.out.printf(" ");
+      }
+		   for (int x = 0; x < y * 2 + 1; x++) {
+				  System.out.printf("X");
+              }
 			System.out.println();
 		}
 	}
